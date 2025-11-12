b6436ca2c5a045eab1f67ff081c26804d3d143a9
XeihtT <miglesiaq@alumnos.unex.es>
2025-11-12 19:13:19 +0100

    Commit acabado de la clase de 12 nov, nuevo codigo integrado, falta hacer cambios en door_detector para hacer el detector de puertas

--- multiroom/src/specificworker.h
+++ multiroom/src/specificworker.h
@@ -52,16 +52,17 @@
 #include "hungarian.h"
 #include <cppitertools/enumerate.hpp>
 #include <cppitertools/zip.hpp>
+#include "time_series_plotter.h"
+#include "nominal_room.h"
+#include "door_detector.h"
+#include "image_processor.h"
 
 
 /**
  * \brief Class SpecificWorker implements the core functionality of the component.
  */
 
-enum class State {
-	FORWARD, TURN_FORWARD, TURN_FOLLOW, SPIRAL, FOLLOW_WALL,
-	LOCALISE, GOTO_ROOM_CENTER, TURN, UPDATE_POSE
-};
+
 
 
 class SpecificWorker : public GenericWorker
@@ -82,6 +83,7 @@ public:
 	~SpecificWorker();
 
 	void JoystickAdapter_sendData(RoboCompJoystickAdapter::TData data);
+
 
 
 
@@ -122,9 +124,10 @@ public slots:
 	void new_target_slot(QPointF);
 
 	//Chocachoca
-	void draw_lidar(const RoboCompLidar3D::TPoints& filter_data, QGraphicsScene* scene);
+	void draw_lidar(const RoboCompLidar3D::TPoints &filtered_points, std::optional<Eigen::Vector2d> center, QGraphicsScene *scene);
+	//void draw_lidar(const RoboCompLidar3D::TPoints &filtered_points, QGraphicsScene *scene);
 
-	/**
+/**
 	 * \brief Initializes the worker one time.
 	 */
 	void initialize();
@@ -158,13 +161,49 @@ private:
      * \brief Flag indicating whether startup checks are enabled.
      */
 	bool startup_check_flag;
-	State state = State::SPIRAL;
 
 	//Random nums:
 	std::random_device rd;
 	std::mt19937 gen;
 	std::uniform_real_distribution<float> rand;
 	std::uniform_int_distribution<int> rand_turn_way;
+
+	//params
+	struct Params
+	{
+		float ROBOT_WIDTH = 460;  // mm
+		float ROBOT_LENGTH = 480;  // mm
+		float MAX_ADV_SPEED = 1000; // mm/s
+		float MAX_ROT_SPEED = 1; // rad/s
+		float MAX_SIDE_SPEED = 50; // mm/s
+		float MAX_TRANSLATION = 500; // mm/s
+		float MAX_ROTATION = 0.2;
+		float STOP_THRESHOLD = 700; // mm
+		float ADVANCE_THRESHOLD = ROBOT_WIDTH * 3; // mm
+		float LIDAR_FRONT_SECTION = 0.2; // rads, aprox 12 degrees
+		// wall
+		float LIDAR_RIGHT_SIDE_SECTION = M_PI/3; // rads, 90 degrees
+		float LIDAR_LEFT_SIDE_SECTION = -M_PI/3; // rads, 90 degrees
+		float WALL_MIN_DISTANCE = ROBOT_WIDTH*1.2;
+		// match error correction
+		float MATCH_ERROR_SIGMA = 150.f; // mm
+		float DOOR_REACHED_DIST = 300.f;
+		std::string LIDAR_NAME_LOW = "bpearl";
+		std::string LIDAR_NAME_HIGH = "helios";
+		QRectF GRID_MAX_DIM{-5000, 2500, 10000, -5000};
+
+		// relocalization
+		float RELOCAL_CENTER_EPS = 300.f;    // mm: stop when |mean| < eps
+		float RELOCAL_KP = 0.002f;           // gain to convert mean (mm) -> speed (magnitude)
+		float RELOCAL_MAX_ADV = 300.f;       // mm/s cap while re-centering
+		float RELOCAL_MAX_SIDE = 300.f;      // mm/s cap while re-centering
+		float RELOCAL_ROT_SPEED = 0.3f;     // rad/s while aligning
+		float RELOCAL_DELTA = 5.0f * M_PI/180.f; // small probe angle in radians
+		float RELOCAL_MATCH_MAX_DIST = 2000.f;   // mm for Hungarian gating
+		float RELOCAL_DONE_COST = 500.f;
+		float RELOCAL_DONE_MATCH_MAX_ERROR = 1000.f;
+	};
+	Params params;
 
 	// graphics
 
@@ -189,8 +228,71 @@ private:
 	//match
 	rc::Hungarian hungarian;
 
+
+	// plotter
+	std::unique_ptr<TimeSeriesPlotter> time_series_plotter;
+	int match_error_graph; // To store the index of the speed graph
+
+
+	// doors
+	DoorDetector door_detector;
+
+	// image processor
+	rc::ImageProcessor image_processor;
+
+	// timing
+	std::chrono::time_point<std::chrono::high_resolution_clock> last_time = std::chrono::high_resolution_clock::now();
+
+	// relocalization
+	bool relocal_centered = false;
+	bool localised = false;
+
+	bool update_robot_pose(const Corners &corners, const Match &match);
+	void move_robot(float adv, float rot, float max_match_error);
+	Eigen::Vector3d solve_pose(const Corners &corners, const Match &match);
+	void predict_robot_pose();
+	std::tuple<float, float> robot_controller(const Eigen::Vector2f &target);
+
+
+	//states
+	// state machine
+	enum class STATE {GOTO_DOOR, ORIENT_TO_DOOR, LOCALISE, GOTO_ROOM_CENTER, TURN, IDLE, CROSS_DOOR};
+	inline const char* to_string(const STATE s) const
+	{
+		switch(s) {
+		case STATE::IDLE:               return "IDLE";
+		case STATE::LOCALISE:           return "LOCALISE";
+		case STATE::GOTO_DOOR:          return "GOTO_DOOR";
+		case STATE::TURN:               return "TURN";
+		case STATE::ORIENT_TO_DOOR:     return "ORIENT_TO_DOOR";
+		case STATE::GOTO_ROOM_CENTER:   return "GOTO_ROOM_CENTER";
+		case STATE::CROSS_DOOR:         return "CROSS_DOOR";
+		default:                        return "UNKNOWN";
+		}
+	}
+	STATE state = STATE::LOCALISE;
+	using RetVal = std::tuple<STATE, float, float>;
+	RetVal goto_door(const RoboCompLidar3D::TPoints &points);
+	RetVal orient_to_door(const RoboCompLidar3D::TPoints &points);
+	RetVal cross_door(const RoboCompLidar3D::TPoints &points);
+	RetVal localise(const Match &match);
+	RetVal goto_room_center(const RoboCompLidar3D::TPoints &points);
+	RetVal update_pose(const Corners &corners, const Match &match);
+	RetVal turn(const Corners &corners);
+	RetVal process_state(const RoboCompLidar3D::TPoints &data, const Corners &corners, const Match &match, AbstractGraphicViewer *viewer);
+
+	// viewer
+	AbstractGraphicViewer *viewer, *viewer_room;
+	QGraphicsPolygonItem *robot_draw, *robot_room_draw;
+
 	//Chocachoca todo lo de abajo:
 	QRectF dimensions;
+
+	enum class State {
+		FORWARD, TURN_FORWARD, TURN_FOLLOW, SPIRAL, FOLLOW_WALL
+	};
+
+	State state_chocachoca = State::SPIRAL;
 
 	AbstractGraphicViewer *viewer1, *viewer2;
 	const int ROBOT_LENGTH = 400;
