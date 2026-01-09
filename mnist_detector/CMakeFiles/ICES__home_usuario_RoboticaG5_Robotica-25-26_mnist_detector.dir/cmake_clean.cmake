2c34b62b4062395abbc9648701602931c094dcd4
XeihtT <miglesiaq@alumnos.unex.es>
2026-01-09 18:31:16 +0100

    Commit de limpieza, se sube el componente de mnist_detector

---
+++ mnist_detector/CMakeFiles/ICES__home_usuario_RoboticaG5_Robotica-25-26_mnist_detector.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_usuario_RoboticaG5_Robotica-25-26_mnist_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mnist_detector/CMakeFiles/ICES__home_xeihtt_robocomp_components_beta-robotica-class_mnist_detector.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_xeihtt_robocomp_components_beta-robotica-class_mnist_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
